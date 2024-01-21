c42313c3c1df593494e8f31fc76e02cdeafb2c18
Halit <oskan96@gmail.com>
2024-01-21 16:19:20 +0000

    Initial commit

---
+++ test/main-trigger-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MainTriggerPipeline from '../lib/main-trigger-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/main-trigger-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MainTriggerPipeline.MainTriggerPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
